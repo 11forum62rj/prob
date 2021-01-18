f4e4f90e59e2aebb5db7d44e5a8a672d7befbcb9
11forum62rj <73180905+11forum62rj@users.noreply.github.com>
2021-01-18 06:29:43 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var dog; 
+var happyDog;
+var database;
+var food;
+var foodStock;
+
+function preload(){
+  dogImg = loadImage("Dog.png");
+}
+
+function setup() {
+  createCanvas(500, 500);
+  
+   Dog = createSprite(250,300,10,10);
+   Dog.addImage("running",dogImg);
+   Dog.scale = 0.1;
+
+
+}
+
+
+function draw() {  
+
+  
+
+  drawSprites();
+ }
+
+ 
+
+
+
+
